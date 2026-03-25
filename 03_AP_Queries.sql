9869a71f3be482c6dbc58e1e6d73e6bbf77fb5f1
MarKee Martin <KEE89.KM@GMAIL.COM>
2026-03-24 20:46:51 -0500

    Update 03_AP_Queries.sql
    add query

--- 03_AP_Queries.sql
+++ 03_AP_Queries.sql
@@ -1,1 +1,24 @@
+-- Join invoices to payments
+SELECT 
+    i.InvoiceID,
+    i.Invoice_Number,
+    i.Invoice_Amount,
+    p.Payment_Amount,
+    p.Payment_Date
+FROM AP_INVOICES i
+LEFT JOIN AP_PAYMENTS p 
+    ON i.InvoiceID = p.InvoiceID;
 
+-- Vendor totals
+SELECT 
+    VendorID, 
+    SUM(Invoice_Amount) AS TotalVendorSpend
+FROM AP_INVOICES
+GROUP BY VendorID;
+
+-- Exceptions by type
+SELECT 
+    Exception_Type, 
+    COUNT(*) AS CountByType
+FROM AP_EXCEPTIONS
+GROUP BY Exception_Type;
